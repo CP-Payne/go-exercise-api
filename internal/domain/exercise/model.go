83163ad70f487d5ad53f1691effcffee6ff61536
CP Payne <charlethical1@gmail.com>
2025-02-12 17:04:19 +0200

    Refactor exercise model to use IDs instead of direct relationships
    Changes made:
    
    Removed Muscle, Equipment, and Split entities from the Exercise model
    Updated Exercise struct to store splitIDs, targetMuscleIDs, and equipmentIDs as slices of uuid.UUID instead of direct relationships
    Modified NewExercise function to create Exercise instances using IDs instead of direct entity objects

--- internal/domain/exercise/model.go
+++ internal/domain/exercise/model.go
@@ -13,34 +13,18 @@ var (
 	ErrInvalidExercise = errors.New("an exercise must have a name")
 )
 
-// Entities
-type Muscle struct {
-	Id   uuid.UUID
-	Name string
-}
-
-type Equipment struct {
-	Id   uuid.UUID
-	Name string
-}
-
-type Split struct {
-	Id   uuid.UUID
-	Name string
-}
-
 // Aggregates
 type Exercise struct {
-	id            uuid.UUID
-	name          string
-	description   string
-	displayImage  url.URL
-	split         []*Split
-	targetMuscles []*Muscle
-	equipment     []*Equipment
-	category      string
-	createdAt     time.Time
-	updatedAt     time.Time
+	id              uuid.UUID
+	name            string
+	description     string
+	displayImage    url.URL
+	splitIDs        []uuid.UUID
+	targetMuscleIDs []uuid.UUID
+	equipmentIDs    []uuid.UUID
+	category        string
+	createdAt       time.Time
+	updatedAt       time.Time
 }
 
 func NewExercise(name, description, category string) (Exercise, error) {
@@ -49,15 +33,15 @@ func NewExercise(name, description, category string) (Exercise, error) {
 	}
 
 	return Exercise{
-		id:            uuid.New(),
-		name:          name,
-		description:   description,
-		displayImage:  url.URL{},
-		split:         make([]*Split, 0),
-		targetMuscles: make([]*Muscle, 0),
-		equipment:     make([]*Equipment, 0),
-		category:      category,
-		createdAt:     time.Now(),
-		updatedAt:     time.Now(),
+		id:              uuid.New(),
+		name:            name,
+		description:     description,
+		displayImage:    url.URL{},
+		splitIDs:        make([]uuid.UUID, 0),
+		targetMuscleIDs: make([]uuid.UUID, 0),
+		equipmentIDs:    make([]uuid.UUID, 0),
+		category:        category,
+		createdAt:       time.Now(),
+		updatedAt:       time.Now(),
 	}, nil
 }
