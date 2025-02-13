8aed1d151f3ac711ca31c19b588f6a5a3450c23b
CP Payne <charlethical1@gmail.com>
2025-02-13 13:44:12 +0200

    refactor(domain/exercise): Add getters and setters to exercise domain model

--- internal/domain/exercise/model.go
+++ internal/domain/exercise/model.go
@@ -10,7 +10,8 @@ import (
 
 // Errors
 var (
-	ErrInvalidExercise = errors.New("an exercise must have a name")
+	ErrInvalidExerciseName = errors.New("an exercise must have a name")
+	ErrInvalidUpdateDate   = errors.New("date cannot be in the past")
 )
 
 // Aggregates
@@ -29,7 +30,7 @@ type Exercise struct {
 
 func NewExercise(name, description, category string) (Exercise, error) {
 	if name == "" {
-		return Exercise{}, ErrInvalidExercise
+		return Exercise{}, ErrInvalidExerciseName
 	}
 
 	return Exercise{
@@ -44,4 +45,72 @@ func NewExercise(name, description, category string) (Exercise, error) {
 		createdAt:       time.Now(),
 		updatedAt:       time.Now(),
 	}, nil
+}
+
+func (e *Exercise) SetName(name string) {
+	e.name = name
+}
+
+func (e *Exercise) GetName() string {
+	return e.name
+}
+
+func (e *Exercise) SetDescription(description string) {
+	e.description = description
+}
+
+func (e *Exercise) GetDescription() string {
+	return e.description
+}
+
+func (e *Exercise) SetDisplayImage(displayImage url.URL) {
+	e.displayImage = displayImage
+}
+
+func (e *Exercise) GetDisplayImage() url.URL {
+	return e.displayImage
+}
+
+func (e *Exercise) AddSplit(splitID uuid.UUID) {
+	e.splitIDs = append(e.splitIDs, splitID)
+}
+
+func (e *Exercise) GetSplits() []uuid.UUID {
+	return e.splitIDs
+}
+
+func (e *Exercise) AddTargetMuscle(targetMuscleID uuid.UUID) {
+	e.targetMuscleIDs = append(e.targetMuscleIDs, targetMuscleID)
+}
+
+func (e *Exercise) GetTargetMuscles() []uuid.UUID {
+	return e.targetMuscleIDs
+}
+
+func (e *Exercise) AddEquipment(equipmentID uuid.UUID) {
+	e.equipmentIDs = append(e.equipmentIDs, equipmentID)
+}
+
+func (e *Exercise) GetEquipments() []uuid.UUID {
+	return e.equipmentIDs
+}
+
+func (e *Exercise) SetCategory(category string) {
+	e.category = category
+}
+
+func (e *Exercise) GetCategory() string {
+	return e.category
+}
+
+func (e *Exercise) GetUpdatedAt() time.Time {
+	return e.updatedAt
+}
+func (e *Exercise) SetUpdatedAt(t time.Time) error {
+
+	if t.Before(time.Now()) {
+		return errors.New("invalid date")
+	}
+	e.updatedAt = t
+	return nil
 }
