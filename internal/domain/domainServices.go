005c9fbb645dcf8976218320ee5c05bb1cc5eb63
CP Payne <charlethical1@gmail.com>
2025-02-21 16:09:51 +0200

    refactor(domain/manager): Add comments to domain service manager

--- internal/domain/domainServices.go
+++ internal/domain/domainServices.go
@@ -5,10 +5,13 @@ import (
 	"github.com/CP-Payne/exercise/internal/interfaces/repositories"
 )
 
+// DomainServices provides access to all domain services
+// from a centralized location
 type DomainServices struct {
 	Muscle muscle.MuscleService
 }
 
+// NewDomainServices creates and initializes all domain service implementations
 func NewDomainServices(r *repositories.Repositories) *DomainServices {
 	return &DomainServices{
 		Muscle: muscle.NewMuscleService(r.Muscles),
